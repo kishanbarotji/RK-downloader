a7729522991f47986de5fcec2769f3fa7c1f8b31
Kishan Barot <kishanbarot32@gmail.com>
2026-04-14 14:10:03 +0530

    Configure project for Vercel deployment

---
+++ build_files.sh
@@ -0,0 +1,4 @@
+echo "Building the project..."
+python3.12 -m pip install -r requirements.txt
+python3.12 manage.py collectstatic --noinput --clear
+echo "Build finished."
--- youtube/wsgi.py
+++ youtube/wsgi.py
@@ -8,22 +8,9 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
-import sys
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'youtube.settings')
 
 application = get_wsgi_application()
-
-# my Added
-
-
-# Add your project directory to the sys.path
-path = '/home/admin/E:/yut/youtube'  
-if path not in sys.path:
-    sys.path.append(path)
-
-os.environ['DJANGO_SETTINGS_MODULE'] = 'youtube.settings'  # Replace 'your_project' with your actual project name
-
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+app = application
